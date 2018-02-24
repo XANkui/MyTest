6495706098f5ca5912da90bca4dcf58fe146b38a
XANkui <1272200579@qq.com>
2018-02-24 15:28:51 +0800

    初次提交

---
+++ Assets/MyCSCallLua.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+using XLua;
+
+public class MyCSCallLua : MonoBehaviour {
+	
+	private LuaEnv luaEnv;
+
+	// Use this for initialization
+	void Start () {
+
+		luaEnv = new LuaEnv ();
+
+		luaEnv.DoString ("require'CSCallLua'");
+
+		IPerson p = luaEnv.Global.Get<IPerson> ("person");
+
+		print ("person.name: " + p.name);
+
+		print ("person.age: " + p.age);
+
+
+		p.name = "Duck";
+
+		luaEnv.DoString ("print(person.name)");
+	}
+	
+	private void OnDestroy() {
+
+		luaEnv.Dispose ();
+	}
+
+	[CSharpCallLua]
+	interface IPerson{
+
+		string name{ get ; set;}
+
+		int age{ get ; set; }
+
+		void Add (int a, int b);
+	}
+}
